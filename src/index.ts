d979fadb3cf96f9791e254eb02356b2b1b8fc142
dharmesh <dharmesh.yadav@wherehouse.io>
2023-05-28 20:41:12 +0530

    created test route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import app from "./app";
+// import app from "./app";
 import dotenv from "dotenv";
-import { dbConfig } from "./configs/database/dbConfig";
+// import { dbConfig } from "./configs/database/dbConfig";
+import express from "express";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ?? 3003;
 
+const app = express();
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("test route");
+});
 
 app.listen(PORT, async () => {
   try {
-    await dbConfig();
+    // await dbConfig();
     console.log(`listning on port - ${PORT}`);
   } catch (error: Error | any) {
     console.error(`[Error] - ${error.message}`);
